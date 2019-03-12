37bf3bbe0a2915a564c2a38004ff284f83345613
Tom Bell <tomb@tomb.io>
2019-03-12 20:21:18 +0000

    Add start of zg main command

---
+++ cmd/zg/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const helpText = `usage: zg [args]
+
+Special options:
+  --help      show this message, then exit
+  --version   show the version number, then exit
+`
+
+var (
+	version = flag.Bool("version", false, "")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+	os.Exit(2)
+}
+
+func validateFlags() {
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "zen-garden %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
+}
